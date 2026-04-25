dd3259158c35af3043dce22ba476c022e6e3a73f
kuzy <kuzeyekinci9@gmail.com>
2026-04-25 13:46:11 +0300

    Update DatabaseHelper

--- kod/src/com/sitemanagement/db/DatabaseHelper.java
+++ kod/src/com/sitemanagement/db/DatabaseHelper.java
@@ -1,5 +1,41 @@
 package com.sitemanagement.db;
 
 public class DatabaseHelper {
+    package com.sitemanagement.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHelper {
+    
+    // VERİTABANI ADI: site_management
+    private static final String URL = "jdbc:mysql://localhost:3306/site_management"; 
+    private static final String USER = "root"; 
+    
+    // şifreyi buraya yaz (Örn: "1234")
+    private static final String PASSWORD = "1234"; 
+
+    private static Connection connection = null;
+
+    // Veritabanı bağlantısını çağır
+    public static Connection getConnection() {
+        try {
+            // Eğer bağlantı yoksa veya kapandıysa yeni bağlantı aç
+            if (connection == null || connection.isClosed()) {
+                
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Sistem: Veritabanı bağlantısı BAŞARILI!");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("HATA: MySQL Driver bulunamadı!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("HATA: Veritabanına bağlanılamadı! ");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
 }
