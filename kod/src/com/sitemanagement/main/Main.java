10583d7393433f1f45b56a957b737cdcccd503cb
autobahnotoban <108894525+autobahnotoban@users.noreply.github.com>
2026-04-26 18:47:43 +0300

    add minimal db test to main.java

--- kod/src/com/sitemanagement/main/Main.java
+++ kod/src/com/sitemanagement/main/Main.java
@@ -1,9 +1,12 @@
 package com.sitemanagement.main;
 
+import com.sitemanagement.db.DatabaseHelper;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DatabaseHelper.getConnection();
 
 	}
 
